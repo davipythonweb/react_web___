b217568e59f7db0616955c711b4ee8c253ca53b2
davipythonweb <galaxys3tab32@gmail.com>
2024-09-02 07:06:54 -0300

    aula 606 finalizada

--- aula_603/src/App.jsx
+++ aula_603/src/App.jsx
@@ -1,8 +1,49 @@
 import React from "react"
 
+// exercicio - aula 606
+
+const alana = {
+  cliente: 'Alana',
+  idade: 25,
+  compras: [
+      {nome: 'Notebook', preco: 'R$ 3000'},
+      {nome: 'Geladeira', preco: 'R$ 3000'},
+      {nome: 'Smartphone', preco: 'R$ 2500'}
+  ],
+  ativa: true
+}
+
+const davi = {
+  cliente: 'Davi',
+  idade: 29,
+  compras: [
+      {nome: 'Violão', preco: 'R$ 1500'},
+      {nome: 'Carro', preco: 'R$ 30000'},
+      {nome: 'Smartphone', preco: 'R$ 4000'}
+  ],
+  ativa: false
+}
+
+
 const App = () => {
+  const dados = alana
+
+  const total = dados.compras
+  .map((item) => Number(item.preco.replace('R$ ', '')))
+  .reduce((a, b) => a + b)
+
   return (
-      <div>App React <button className="btn btn-outline-primary">Click</button></div>
+      <div>
+        <p>Nome: {dados.cliente}</p>
+        <p>Idade: {dados.idade}</p>
+        <p>Situação:
+          <span style={{color:dados.ativa ? 'green' : 'red'}}>
+            {dados.ativa ? 'Ativa' : 'Inativa'}
+          </span>
+        </p>
+        <p>Total: R$ {total}</p>
+        {total > 10000 && <p>Você untrapassou o limite de dez mil reais!</p>}
+      </div>
   )
 }
 
---
+++ aula_603/src/exercicio_606.js
@@ -0,0 +1,31 @@
+// Mostre os dados da aplicação, 
+// se a situação estiver ativa pinte de verde, inativa vermelho
+// se o gasto for maior que 10000 mostre uma mensagem
+
+const luana = {
+    cliente: 'Alana',
+    idade: 25,
+    compras: [
+        {nome: 'Notebook', preco: 'R$ 5000'},
+        {nome: 'Geladeira', preco: 'R$ 3000'},
+        {nome: 'Smartphone', preco: 'R$ 2500'}
+    ],
+    ativa: true
+}
+
+const Davi = {
+    cliente: 'Davi',
+    idade: 29,
+    compras: [
+        {nome: 'Violão', preco: 'R$ 1500'},
+        {nome: 'Bike', preco: 'R$ 3000'},
+        {nome: 'Smartphone', preco: 'R$ 4000'}
+    ],
+    ativa: false
+}
+
+const App = () => {
+    const dados = luana
+
+    return <div></div>
+}
