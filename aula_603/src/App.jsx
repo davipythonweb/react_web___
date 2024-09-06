2a1fa3f603058e8de7c2f0cb15416761c57dfa21
davipythonweb <galaxys3tab32@gmail.com>
2024-09-06 02:12:34 -0300

    aula 606 finalizada

--- aula_603/src/App.jsx
+++ aula_603/src/App.jsx
@@ -28,20 +28,25 @@ const davi = {
 const App = () => {
   const dados = alana
 
+  // fazendo os calculos, limpar os arrais com o map passando o item como callback, usando o replace para trocar os Crifores por espaços vazios e tranformando em numero com number. fazendo a soma dos numeros do arrai com o reduce, recebendo o valor anterior + o valor atual.
   const total = dados.compras
   .map((item) => Number(item.preco.replace('R$ ', '')))
   .reduce((a, b) => a + b)
 
   return (
+    // variaveus com os dados dos clientes
       <div>
         <p>Nome: {dados.cliente}</p>
         <p>Idade: {dados.idade}</p>
+        {/* verificando a situação do cliente, usando operadores ternarios para mudar a situaçao ,e para mudar de cores. Usando a tag style para as cores */}
         <p>Situação:
           <span style={{color:dados.ativa ? 'green' : 'red'}}>
             {dados.ativa ? 'Ativa' : 'Inativa'}
           </span>
         </p>
+        {/* fazendo uma mensagem para verificar se o valor eh maior que 10000, utilizando o verificador => && . */}
         <p>Total: R$ {total}</p>
+        {/* quando a expressao eh verdadeira eh criado o elemento JSX. */}
         {total > 10000 && <p>Você untrapassou o limite de dez mil reais!</p>}
       </div>
   )
