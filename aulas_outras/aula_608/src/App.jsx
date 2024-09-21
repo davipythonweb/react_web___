7b763721dca8c6fb242119639372fe3524299369
davypythonweb <galaxys3tab32@gmail.com>
2024-09-21 13:09:18 -0300

    update app.jsx

--- aulas_outras/aula_608/src/App.jsx
+++ aulas_outras/aula_608/src/App.jsx
@@ -3,55 +3,48 @@ import viteLogo from '/vite.svg'
 
 // exercicio - aula 608
 
-const alana = {
-  cliente: 'Alana',
-  idade: 25,
-  compras: [
-      {nome: 'Notebook', preco: 'R$ 3000'},
-      {nome: 'Geladeira', preco: 'R$ 3000'},
-      {nome: 'Smartphone', preco: 'R$ 2500'}
-  ],
-  ativa: true
+const produtos = [
+  {
+      id: 1,
+      nome: 'Smartphone',
+      preco: 'R$ 2000',
+      cores: ['#29d8d5', '#252a34', '#fc3766'],
+  },
+  {
+    id: 2,
+    nome: 'Notebook',
+    preco: 'R$ 3000',
+    cores: ['#ffd045', '#d4394b', '#f37c59'],
+},
+{
+    id: 3,
+    nome: 'Tablet',
+    preco: 'R$ 1500',
+    cores: ['#365069', '#47c1c8', '#f95786']
 }
-
-const davi = {
-  cliente: 'Davi',
-  idade: 29,
-  compras: [
-      {nome: 'Violão', preco: 'R$ 1500'},
-      {nome: 'Carro', preco: 'R$ 30000'},
-      {nome: 'Smartphone', preco: 'R$ 4000'}
-  ],
-  ativa: false
-}
-
+]
 
 const App = () => {
-  const dados = alana
+  const dados = produtos.filter(
+    ({ preco }) => Number(preco.replace('R$ ', '')) > 1500,
+)
 
-  // fazendo os calculos, limpar os arrais com o map passando o item como callback, usando o replace para trocar os Crifores por espaços vazios e tranformando em numero com number. fazendo a soma dos numeros do arrai com o reduce, recebendo o valor anterior + o valor atual.
-  const total = dados.compras
-  .map((item) => Number(item.preco.replace('R$ ', '')))
-  .reduce((a, b) => a + b)
-
-  return (
-    // variaveus com os dados dos clientes
-      <div>
-        <p>Nome: {dados.cliente}</p>
-        <p>Idade: {dados.idade}</p>
-        {/* verificando a situação do cliente, usando operadores ternarios para mudar a situaçao ,e para mudar de cores. Usando a tag style para as cores */}
-        <p>Situação:
-          <span style={{color:dados.ativa ? 'green' : 'red'}}>
-            {dados.ativa ? 'Ativa' : 'Inativa'}
-          </span>
-        </p>
-        {/* fazendo uma mensagem para verificar se o valor eh maior que 10000, utilizando o verificador => && . */}
-        <p>Total: R$ {total}</p>
-        {/* quando a expressao eh verdadeira eh criado o elemento JSX. */}
-        {total > 10000 && <p>Você untrapassou o limite de dez mil reais!</p>}
+return (
+      <section>
+        {/* desestruturando o array com map. */}
+      {dados.map(({ id, nome, preco, cores}) =>
+      <div key={{id}}>
+          <h1>{nome}</h1>
+          <p>Preço: {preco}</p>
+          <ul>
+              {cores.map(cor => (
+                  <li style={{backgroundColor: cor, color: 'white'}} key={cor}>{cor}</li>
+              ))}
+              </ul>
       </div>
-  )
-}
+    )}
+    </section>
+    )
+  }
 
 export default App
-
