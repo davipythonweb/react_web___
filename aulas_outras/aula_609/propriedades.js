ca53163aa78233582c7a712cd737b63265944c01
davypythonweb <galaxys3tab32@gmail.com>
2024-09-25 02:00:42 -0300

    create aula 611

---
+++ aulas_outras/aula_609/propriedades.js
@@ -0,0 +1,18 @@
+// Propiedades
+
+// Assim como uma funçao pode receber argumentos,
+// podemos tambem passar argumentos aos componentes.
+// Esses sao conhecimentos como propriedades ou props.
+import React from 'react'
+
+const Titulo = (props) = {
+	return <h1>{props.texto}</h1>
+}
+const App = () => {
+	return (
+		<section>
+			<Titulo texto="Meu Primeiro Títuo" />
+			<Titulo texto="Meu Segundo Títuo" />
+		</section>
+	)
+}
