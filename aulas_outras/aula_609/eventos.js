c753c918746693567204fb9cc2c006140ce43e02
davypythonweb <galaxys3tab32@gmail.com>
2024-09-24 21:59:50 -0300

    update aula 609

---
+++ aulas_outras/aula_609/eventos.js
@@ -0,0 +1,7 @@
+// Eventos
+
+// Podemos atribuir eventos diretamente aos elementos JSX como
+// um atributo. Os Eventos sao sintáticos, ou seja, sao criados
+// pelo própio React porem seguindo as especificaçoes da W3C
+//(e funcionam igualmente nos diversos browsers que o React
+// suporta )
