db86094d05e1bd2658944b800c8e21de7ff06f32
davypythonweb <galaxys3tab32@gmail.com>
2024-09-24 23:06:49 -0300

    create packeges aula 609

--- aulas_outras/aula_609/eventos.js
+++ aulas_outras/aula_609/eventos.js
@@ -1,3 +1,6 @@
+import React from 'react'
+import viteLogo from '/vite.svg'
+
 // Eventos
 
 // Podemos atribuir eventos diretamente aos elementos JSX como
@@ -5,3 +8,18 @@
 // pelo própio React porem seguindo as especificaçoes da W3C
 //(e funcionam igualmente nos diversos browsers que o React
 // suporta )
+
+const App = () => {
+  function handleClick(event) {
+    alert('Comprou: ' + event.target.innerText)
+  }
+}
+
+return (
+  <div>
+    <button onClick={hadleClick}>Camisa</button>
+    <button onClick={hadleClick}>Bermuda</button>
+  </div>
+  )
+
+export default App
