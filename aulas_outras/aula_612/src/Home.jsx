807e2f72dba061d901f1ed9c3be7c23cc41d5f9e
davypythonweb <galaxys3tab32@gmail.com>
2024-09-28 22:24:37 -0300

    desafio aula 612

---
+++ aulas_outras/aula_612/src/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Titulo from './Titulo'
+
+const Home = () => {
+	return (
+	<section>
+		<Titulo texto="Home" />
+			<p>Essa eh a home</p>
+			<h1>Home</h1>
+		</section>
+)
+}
+export default Home
---
+++ aulas_outras/aula_612/src/Produtos.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Titulo from './Titulo'
+
+const 	Produtos = ()  => {
+	return (
+		<section>
+			<Titulo texto="Produtos" />
+		</section>
+)
+}
+export default Produtos
