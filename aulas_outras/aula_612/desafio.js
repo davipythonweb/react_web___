ec0c3b80ffa225430bfe4402ce775af7daa14a3a
davypythonweb <galaxys3tab32@gmail.com>
2024-09-28 21:57:47 -0300

    create aula 612

---
+++ aulas_outras/aula_612/desafio.js
@@ -0,0 +1,10 @@
+// Replique a interface como apresentada na aula
+// utilize o array abaixo para mostrar os produtos
+// Quebre em componetes o que precisar ser reutilizado
+// Dica: Const{ pathaname} = window.location; (puxa o caminho do)
+
+const produtos = [
+	{nome: 'Notebook', propiedades: ['4gb ram', '120gb']},
+	{nome: 'Smartphone', propiedades: ['3gb ram', '32gb']}
+]	
+
---
+++ aulas_outras/aula_612/src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import viteLogo from '/vite.svg'
+
+function App() {
+
+  return 
+}
+
+export default App
