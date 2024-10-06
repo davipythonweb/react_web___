11cfe06fb6b1a74d90f0ea581b6da4e797e2d4aa
davypythonweb <galaxys3tab32@gmail.com>
2024-10-06 17:23:49 -0300

    exercicio 615-inicio

--- aulas_outras/aula_615/src/App.jsx
+++ aulas_outras/aula_615/src/App.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import viteLogo from '/vite.svg'
+import Produto from './Produto'
 
 const App = () => {
-  
+  const [dados, setDados] = React.useState(null)
 
-  return 
+  function handleClick(event) {
+    console.info(event.target.innerText)
+  }
+  return (
+    <div>
+    <button class='btn btn-danger' style={{margin: '.5rem'}} onClick={handleClick}>notebook</button>
+    <button class='btn btn-success' style={{margin: '.5rem'}} onClick={handleClick}>smartphone</button>
+    <button class='btn btn-primary' style={{margin: '.5rem'}} onClick={handleClick}>tablet</button>
+      <Produto />
+    </div>
+    )
 }
 
 export default App
