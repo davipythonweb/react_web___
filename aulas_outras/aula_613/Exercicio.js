c73c5b8fa34d4ceb406ec871aab5a6241ca305a6
davypythonweb <galaxys3tab32@gmail.com>
2024-10-06 16:33:08 -0300

    create exercicio aula 615

---
+++ aulas_outras/aula_613/Exercicio.js
@@ -0,0 +1,13 @@
+// Exercicio Aula 615
+
+
+//Os links abaixo puxam dados de um produto em formato JSOM
+
+// https://ranekapi.origamid.dev/json/api/produto/tablet
+// https://ranekapi.origamid.dev/json/api/produto/smartphone
+// https://ranekapi.origamid.dev/json/api/produto/notebook
+
+// Crie uma interface com 3 botoes, um para cada produto.
+// Ao clicar n botao faça um fetch a api e mostre os dados do produto
+// mostre apenas um produto por vez.
+// Mostre a mensagem carregando... enquanto o fetch eh realizado
