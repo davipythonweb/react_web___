df94cb8710331bd6017fd396b3202c8079daa1ed
davypythonweb <galaxys3tab32@gmail.com>
2024-10-05 21:11:48 -0300

    Hooks com react- aula 613

--- aulas_outras/aula_613/Hooks.js
+++ aulas_outras/aula_613/Hooks.js
@@ -14,3 +14,48 @@ const App = () => {
 )
 }
 export default App
+
+
+
+//Hooks
+
+// Os Hooks sao funçoes especiais do React que permitem
+// controlarmos o estado e o ciclo de vida de componetes
+// funcionais. Isso antes só era possivel com classes.
+
+import React from 'react'
+import useState from 'react'
+
+const Exemplo = () => {
+	const [ativo, setAtivo] = React.useState(true)
+
+	return (
+		<button onClick={() => setAtivo(!ativo)}>
+			{ativo ? 'Botao Ativo' : 'Botao Inativo'}
+		</button>
+		)
+}
+
+//segundo exemplo de Hook:
+
+import React from 'react'
+import viteLogo from '/vite.svg'
+
+
+const App = () => {
+  const ativoHook = React.useState(false)
+  const ativoValor = ativoHook[0]
+  const atualizaValor = ativoHook[1]
+  console.info(ativoValor)
+
+  function handleClick() { 
+    atualizaValor(!ativoValor) 
+  } 
+
+return ( 
+  //usando um verificador ternario, modificando o valor atual utilizando a funcao 
+  <button onClick={handleClick}>{ativoValor ? 'Botao Ativo' : 'Botao Inativo'}</button> 
+  )
+
+}
+export default App
--- aulas_outras/aula_613/src/App.jsx
+++ aulas_outras/aula_613/src/App.jsx
@@ -1,14 +1,19 @@
 import React from 'react'
-
 import viteLogo from '/vite.svg'
 
 
 const App = () => {
-  const ativo = true
+  const [ativo, setAtivo] = React.useState(false)
+  console.info(ativo)
 
-  return (
-    //usando um verificador ternario
-    <button disabled={!ativo}>{ativo ? 'Botao Ativo' : 'Botao Inativo'}</button>
-)
+  function handleClick() { 
+    setAtivo(!ativo) 
+  } 
+
+return ( 
+  //usando um verificador ternario, modificando o valor atual utilizando a funcao 
+  <button onClick={handleClick}>{ativo ? 'Botao Ativo' : 'Botao Inativo'}</button> 
+  )
+
 }
 export default App
