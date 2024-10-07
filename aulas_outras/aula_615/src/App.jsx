4ab56672fabaab96064d6251eda3ff5fd6519d73
davypythonweb <galaxys3tab32@gmail.com>
2024-10-07 00:34:00 -0300

    exercicio615 finalizado

--- aulas_outras/aula_615/src/App.jsx
+++ aulas_outras/aula_615/src/App.jsx
@@ -5,23 +5,33 @@ import Produto from './Produto'
 const App = () => {
   const [dados, setDados] = React.useState(null)
 
+  const [carregando, setCarregando] = React.useState(null)
+
   async function handleClick(event) {
+
+    setCarregando(true)
+
     const response = await fetch(
       `https://ranekapi.origamid.dev/json/api/produto/${event.target.
       innerText}`
       )
     const json = await response.json()
     setDados(json)
+    setCarregando(false)
+
     console.info(json)
   }
 
   return (
     <div>
-    <button className='btn btn-danger' style={{margin: '.5rem'}} onClick={handleClick}>notebook</button>
-    <button className='btn btn-success' style={{margin: '.5rem'}} onClick={handleClick}>smartphone</button>
-    <button className='btn btn-primary' style={{margin: '.5rem'}} onClick={handleClick}>tablet</button>
-      {/* verificar  se dados existe, se for verdadeiro, mostrar produto */}
-      {dados && <Produto dados={dados}/>}
+      <button className='btn btn-danger' style={{margin: '.5rem'}} onClick={handleClick}>notebook</button>
+      <button className='btn btn-success' style={{margin: '.5rem'}} onClick={handleClick}>smartphone</button>
+      <button className='btn btn-primary' style={{margin: '.5rem'}} onClick={handleClick}>tablet</button>
+
+      {carregando && <p>Carregando...</p>}
+
+      {/* se carregando for falso e verificar  se dados existe, se for verdadeiro, mostrar produto */}
+      {!carregando && dados && <Produto dados={dados}/>}
     </div>
     )
 }
--- aulas_outras/aula_615/src/Produto.jsx
+++ aulas_outras/aula_615/src/Produto.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Produto = ({ dados }) => {
 	return <div>
-		<h1>{dados.nome}</h1>
-		<p>R$ {dados.preco}</p>
-		<img src={dados.fotos[0].src} alt={dados.fotos[0].titulo} />
+		<h1 style={{ margin: '.9rem'}}>{dados.nome}</h1>
+		<p style={{ margin: '.9rem'}}>R$ {dados.preco}</p>
+		<img style={{ margin: '.9rem'}} src={dados.fotos[0].src} alt={dados.fotos[0].titulo} />
 	</div>
 }
 
